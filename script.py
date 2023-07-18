1c67a8b2e2ed7115604ede7ab45dc84b9b8f67f6
Yegor Arndt <egor.kurochkin@int.alloysoftware.com>
2023-07-18 23:15:11 +0400

    Finish sending part

--- script.py
+++ script.py
@@ -1,10 +1,30 @@
 from pyrogram import Client
 
-api_id = 'your_api_id'
-api_hash = 'your_api_hash'
-chat_ids = ['chat_id1', 'chat_id2', 'chat_id3', ...] 
-message = "Hello, it's me"
+api_id = 'this you dont need to know'
+api_hash = 'this either'
+chat_ids = ['khe-khe'] 
+message = "any message"
 
 with Client("my_account", api_id, api_hash) as app:
     for chat_id in chat_ids:
         app.send_message(chat_id, message)
+
+# To scan 
+# Finder.vc 1143063102 @theyseeku
+# Работа для программистов 1278223896
+# IT Jobs Hub 1931779883 @inter_it_job
+# Pro.* Jobs 1101757939 @ProCxxJobs
+# GetIT Jobs 1092674389 @Getitrussia
+# Работа в ИТ 1091870362 @workayte
+# Pro.JVM Jobs 1091028994 @jvmjobs
+# Profunctor Jobs 1428193228 @profunctor_jobs
+
+# ____________________________
+
+# To apply 
+# mobile dev jobs 1054966032 @mobile_jobs
+# CV & Job by HR Breakfast 1205047805 @hr_breakfast_emergency
+# IT вакансии Алерон делится 1598488312
+# IT Вакансии {Разработка...} 1262748732 @jobGeeks
+# Jobs_IT 1048585467 @jobs_it
+# Программирование, вакансии, резюме 1334021763 @RuDotNetJobs
